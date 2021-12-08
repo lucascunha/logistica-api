a44717abc7718283b934ba0df29ae04237e18b70
Lucas Cunha <lucascunha@uol.com.br>
2021-12-08 10:01:53 -0300

    adicao de novas tabelas e criacao dos controladores

--- src/main/java/com/lucascunha/logistica/domain/model/Cliente.java
+++ src/main/java/com/lucascunha/logistica/domain/model/Cliente.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -23,8 +26,18 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@Column(name = "nome") // opcional, caso não declarado, irá considerar o nome do artibuto
+	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 	
 	
